449d3f7485e3e66b753de1a0e4a539720835569e
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-06-16 16:31:37 +0545

    test

---
+++ game.js
@@ -0,0 +1,31 @@
+//Trigger when "start" button is clicked
+document.getElementById('start').onclick = function () {
+    var horses = document.getElementsByClassName("horse");
+    //Running Animation for every horse
+    for (const horse of horses) {
+        horse.classList.add("runRight");
+        horse.classList.remove("standRight");
+        var x = 20;
+        moveRight(horse, x);
+    }
+};
+
+function moveRight(horse, x) {
+    setTimeout(() => {
+        x++;
+        horse.style.left = x + "vw";
+        if (x < 82.5) {
+            moveRight(horse, x);
+        }
+    }, 500 / (Math.random() * 10 + 10));
+}
+
+function moveTop(horse, y) {
+  setTimeout(() => {
+    y++;
+    horse.style.left = y + "vw";
+    if (x < 82.5) {
+      moveRight(horse, x);
+    }
+  }, 500 / (Math.random() * 10 + 10));
+}
