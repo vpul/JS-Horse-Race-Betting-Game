ec560aed89847fa7a9056848e86c37c5bd659a04
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-06-16 16:23:07 +0545

    Move Right

--- assignment2/game.js
+++ assignment2/game.js
@@ -5,5 +5,17 @@ document.getElementById('start').onclick = function () {
     for (const horse of horses) {
         horse.classList.add("runRight");
         horse.classList.remove("standRight");
+        var x = 20;
+        moveRight(horse, x);
     }
 };
+
+function moveRight(horse, x) {
+    setTimeout(() => {
+        x++;
+        horse.style.left = x + "vw";
+        if (x < 82.5) {
+            moveRight(horse, x);
+        }
+    }, 500 / (Math.random() * 10 + 10));
+}
