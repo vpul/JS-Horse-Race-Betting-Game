4a84c92e0f9519b42bb993458e1b2af87baae5b4
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-06-16 15:01:19 +0545

    Horse running animation

--- assignment2/game.js
+++ assignment2/game.js
@@ -1,3 +1,9 @@
+//Trigger when "start" button is clicked
 document.getElementById('start').onclick = function () {
-    alert('Click just happened');
+    var horses = document.getElementsByClassName("horse");
+    //Running Animation for every horse
+    for (const horse of horses) {
+        horse.classList.add("runRight");
+        horse.classList.remove("standRight");
+    }
 };
